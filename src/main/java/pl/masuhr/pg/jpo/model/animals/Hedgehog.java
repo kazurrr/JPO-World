44499db78d2cfc5e082a46c16a5c692794310d17
kazurrr <Ka43ro21>
2016-10-16 13:26:58 +0200

    Added templates for remaining organisms

---
+++ src/main/java/pl/masuhr/pg/jpo/model/animals/Hedgehog.java
@@ -0,0 +1,8 @@
+package pl.masuhr.pg.jpo.model.animals;
+
+/**
+ * JPO-Zaliczenie
+ * Created by karol on 16.10.2016.
+ */
+public class Hedgehog {
+}
---
+++ src/main/java/pl/masuhr/pg/jpo/model/plants/Guarana.java
@@ -0,0 +1,8 @@
+package pl.masuhr.pg.jpo.model.plants;
+
+/**
+ * JPO-Zaliczenie
+ * Created by karol on 16.10.2016.
+ */
+public class Guarana {
+}
