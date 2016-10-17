7e6e2d42edec6f977d48ea5573510d2d8e0691da
kazurrr <Ka43ro21>
2016-10-17 21:24:39 +0200

    Added Belladonna

--- src/main/java/pl/masuhr/pg/jpo/model/plants/Belladonna.java
+++ src/main/java/pl/masuhr/pg/jpo/model/plants/Belladonna.java
@@ -1,8 +1,33 @@
 package pl.masuhr.pg.jpo.model.plants;
 
+import pl.masuhr.pg.jpo.controller.Logger;
+import pl.masuhr.pg.jpo.controller.World;
+import pl.masuhr.pg.jpo.model.Organism;
+import pl.masuhr.pg.jpo.model.Plant;
+
+import java.awt.*;
+
 /**
  * JPO-Zaliczenie
  * Created by karol on 16.10.2016.
  */
-public class Belladonna {
+public class Belladonna extends Plant {
+    private Logger logger = Logger.getInstance();
+
+    public Belladonna() {
+        super(null, null);
+    }
+
+    public Belladonna(World myWorld, Point position) {
+        super(myWorld, position);
+    }
+
+    @Override
+    public void collisionWith(Organism organism) {
+        myWorld.removeOrganism(this);
+        myWorld.removeOrganism(organism);
+
+        logger.info(this.getPosition(), organism.draw() + " ate " + this.draw() + ".\n"
+                + organism.draw() + " also died.");
+    }
 }
